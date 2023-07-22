d55a2fdf70969a98788551920390d397e0b779f1
Angelina <agremina@gmail.com>
2023-07-21 22:15:26 -0400

    Updated Navbar component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,26 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 import Education from './Components/Education';
 import Footer from './Components/Footer';
+import { Element } from 'react-scroll';
 
 
 function App() {
     return (
         <div className='components'>
             <Navbar /> 
+     
+            <Element name ="home">  
             <Header/>
+            </Element>
+
             <Home/>
-           <Education/> 
+
+            <Element name = "education">
+            <Education/> 
+
+            </Element>
+
+
            <Footer/> 
 
         </div>
